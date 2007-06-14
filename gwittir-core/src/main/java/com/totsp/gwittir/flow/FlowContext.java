7939f87e3f5d33021d445a76729469baa190ec16
kebernet <kebernet@d620f1b1-8330-0410-a80d-d7860aac830e>
2007-06-14 20:36:20 +0000

    git-svn-id: http://gwittir.googlecode.com/svn/trunk@6 d620f1b1-8330-0410-a80d-d7860aac830e

--- gwittir-core/src/main/java/com/totsp/gwittir/flow/FlowContext.java
+++ gwittir-core/src/main/java/com/totsp/gwittir/flow/FlowContext.java
@@ -10,6 +10,7 @@
 package com.totsp.gwittir.flow;
 
 import com.google.gwt.core.client.GWT;
+import com.totsp.gwittir.Action;
 import com.totsp.gwittir.ui.BoundWidget;
 import java.util.HashMap;
 
@@ -20,6 +21,7 @@ import java.util.HashMap;
 public class  FlowContext {
     
     private final HashMap destinations = new HashMap();
+    private final HashMap actions = new HashMap();
     
     /** Creates a new instance of FlowContext */
     public FlowContext() {
@@ -36,15 +38,43 @@ public class  FlowContext {
         return this;
     }
     
+     public FlowContext add( String name, BoundWidgetProvider provider ){
+        destinations.put( name, provider );
+        return this;
+    }
+     
+     public FlowContext add( String name, BoundWidget widget, Action action ){
+        add( name, widget );
+        actions.put( name, action );
+        return this;
+    }
+    
+    public FlowContext add( String name, Class boundWidgetClass, Action action ){
+        add( name, boundWidgetClass );
+        actions.put( name, action );
+        return this;
+    }
+    
+     public FlowContext add( String name, BoundWidgetProvider provider, Action action ){
+        add( name, provider );
+        actions.put( name, action );
+        return this;
+    }
+    
     public BoundWidget get( String name ){
         Object value =  destinations.get(name);
+        BoundWidget ret = null;
         if( value instanceof Class ){
-            return (BoundWidget) GWT.create( (Class) value );
+            ret = (BoundWidget) GWT.create( (Class) value );
+        } else if( value instanceof BoundWidgetProvider ){
+            ret = ((BoundWidgetProvider) value).get();
+        } else {
+            ret =  (BoundWidget) destinations.get(name);
         }
-        if( value instanceof BoundWidgetProvider ){
-            return ((BoundWidgetProvider) value).get();
+        if( ret != null && actions.get( name ) != null ){
+            ret.setAction( (Action) actions.get(name) );
         }
-        return (BoundWidget) destinations.get(name);
+        return ret;
     }
     
     
