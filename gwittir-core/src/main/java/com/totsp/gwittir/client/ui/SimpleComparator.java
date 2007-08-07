5e5b1a7d7a176a345b791b221e57119c050fa718
kebernet <kebernet@d620f1b1-8330-0410-a80d-d7860aac830e>
2007-08-07 21:33:48 +0000

    ""
    git-svn-id: http://gwittir.googlecode.com/svn/trunk@95 d620f1b1-8330-0410-a80d-d7860aac830e

--- gwittir-core/src/main/java/com/totsp/gwittir/client/ui/SimpleComparator.java
+++ gwittir-core/src/main/java/com/totsp/gwittir/client/ui/SimpleComparator.java
@@ -33,6 +33,9 @@ public class SimpleComparator implements Comparator, Serializable {
     }
     
     public int compare(Object o1, Object o2) {
+        if( o1 instanceof Comparable && o2 instanceof Comparable ){
+            return ((Comparable) o1).compareTo( o2 );
+        }
         if((o1 == o2) || ((o1 != null) && (o2 != null) && o1.equals(o2))) {
             return 0;
         } else if((o1 != null) && (o2 != null)) {
