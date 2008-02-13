85e6fa18bb7acc41c1aa03693c94d6e78ad6e014
charlie.collins <charlie.collins@d620f1b1-8330-0410-a80d-d7860aac830e>
2008-02-13 17:33:32 +0000

    formatting, comments
    git-svn-id: http://gwittir.googlecode.com/svn/trunk@221 d620f1b1-8330-0410-a80d-d7860aac830e

--- gwittir-core-sample/src/main/java/com/totsp/gwittir/example/client/ContactBrowser.java
+++ gwittir-core-sample/src/main/java/com/totsp/gwittir/example/client/ContactBrowser.java
@@ -17,10 +17,10 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
-
 package com.totsp.gwittir.example.client;
 
 import com.google.gwt.user.client.ui.VerticalPanel;
+
 import com.totsp.gwittir.client.action.Action;
 import com.totsp.gwittir.client.ui.AbstractBoundWidget;
 import com.totsp.gwittir.client.ui.Button;
@@ -28,47 +28,46 @@ import com.totsp.gwittir.client.ui.Label;
 import com.totsp.gwittir.client.ui.table.BoundTable;
 import com.totsp.gwittir.client.ui.util.BoundWidgetTypeFactory;
 
+
 /**
+ * DOCUMENT ME!
  *
  * @author cooper
  */
-public class ContactBrowser extends AbstractBoundWidget{
-    
+public class ContactBrowser extends AbstractBoundWidget {
     private Object value;
     VerticalPanel p = new VerticalPanel();
     BoundWidgetTypeFactory factory = new BoundWidgetTypeFactory();
-    BoundTable contacts = new BoundTable( BoundTable.SCROLL_MASK +
-            BoundTable.HEADER_MASK +
-            BoundTable.NO_SELECT_CELL_MASK +
-            BoundTable.NO_SELECT_COL_MASK +
-            BoundTable.SORT_MASK, factory );
+
+    // use a "BoundTable" with bitmasks for various options
+    BoundTable contacts = new BoundTable(BoundTable.SCROLL_MASK +
+            BoundTable.HEADER_MASK + BoundTable.NO_SELECT_CELL_MASK +
+            BoundTable.NO_SELECT_COL_MASK + BoundTable.SORT_MASK, factory);
     Button newContact = new Button("New Contact");
-    Label l = new Label( "Browse Contacts");
-    
-    /** Creates a new instance of ContactBrowser */
+    Label l = new Label("Browse Contacts");
+
+/** Creates a new instance of ContactBrowser */
     public ContactBrowser() {
-        factory.add( String.class, BoundWidgetTypeFactory.LABEL_PROVIDER );
+        factory.add(String.class, BoundWidgetTypeFactory.LABEL_PROVIDER);
         contacts.setSize("500px", "400px");
-        super.initWidget( p );
-        p.add( l );
-        p.add( contacts );
-        p.add( newContact );
-        
+        super.initWidget(p);
+        p.add(l);
+        p.add(contacts);
+        p.add(newContact);
     }
-    
+
     public Object getValue() {
         return value;
     }
-    
+
     public void setValue(Object value) {
         this.value = value;
     }
-    
+
     // when the action is set on this Widget it is applied to the contacts table AND the newContact button
-    public void setAction(Action action ){
-        super.setAction( action );
-        contacts.setAction( action );
-        newContact.setAction( action );
+    public void setAction(Action action) {
+        super.setAction(action);
+        contacts.setAction(action);
+        newContact.setAction(action);
     }
-    
 }
