629dccc3523d8d209859700d009d841f8154e858
kebernet <kebernet@d620f1b1-8330-0410-a80d-d7860aac830e>
2008-05-31 18:24:48 +0000

    NB has SCREWED my svn commits. Trying to clean up.
    git-svn-id: http://gwittir.googlecode.com/svn/trunk@241 d620f1b1-8330-0410-a80d-d7860aac830e

---
+++ gwittir-core/src/main/java/com/googlecode/gwtx/java/beans/emul/java/beans/PropertyChangeListener.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2007 Sandy McArthur, Jr.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package java.beans;
+
+import java.util.EventListener;
+
+public interface PropertyChangeListener extends EventListener {
+    /*
+     * This file is based on code from the Apache Harmony Project.
+     * http://svn.apache.org/repos/asf/harmony/enhanced/classlib/trunk/modules/beans/src/main/java/java/beans/PropertyChangeListener.java
+     *
+     * This file has been siginificantly modified for the target enviroment of a
+     * browser but the Harmony layout and structure has been kept to try to help
+     * make future re-syncs easier.
+     */
+
+    public void propertyChange(PropertyChangeEvent event);
+}
