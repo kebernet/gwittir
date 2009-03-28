582e019978659d9a0bc3541d7028b5927e13b5ab
kebernet <kebernet@d620f1b1-8330-0410-a80d-d7860aac830e>
2009-03-28 16:48:57 +0000

    Just formatting and licensing
    git-svn-id: http://gwittir.googlecode.com/svn/trunk@262 d620f1b1-8330-0410-a80d-d7860aac830e

--- gwittir-core/src/main/java/com/totsp/gwittir/client/util/impl/WindowContextPersister.java
+++ gwittir-core/src/main/java/com/totsp/gwittir/client/util/impl/WindowContextPersister.java
@@ -5,6 +5,9 @@
 
 package com.totsp.gwittir.client.util.impl;
 
+import com.totsp.gwittir.client.util.WindowContext.WindowContextItem;
+import java.util.Map;
+
 /**
  *
  * @author kebernet
@@ -13,8 +16,8 @@ public abstract class WindowContextPersister {
 
     public abstract int getByteLimit();
 
-    public abstract String getWindowContextData();
+    public abstract Map<Class<? extends WindowContextItem>, WindowContextItem> getWindowContextData();
 
-    public abstract void storeWindowContextData(String windowContextData);
+    public abstract void storeWindowContextData(Map<Class<? extends WindowContextItem>, WindowContextItem> windowContextData);
 
 }
--- gwittir-core/src/main/java/com/totsp/gwittir/client/util/impl/WindowContextPersisterHTML5.java
+++ gwittir-core/src/main/java/com/totsp/gwittir/client/util/impl/WindowContextPersisterHTML5.java
@@ -5,6 +5,9 @@
 
 package com.totsp.gwittir.client.util.impl;
 
+import com.totsp.gwittir.client.util.WindowContext.WindowContextItem;
+import java.util.Map;
+
 /**
  * This is a persistence mechanism for newer versions of WebKit based on the
  * HTML SQL storage:
@@ -13,7 +16,22 @@ package com.totsp.gwittir.client.util.impl;
  *
  * @author kebernet
  */
-public class WindowContextPersisterHTML5 {
+public class WindowContextPersisterHTML5 extends WindowContextPersister {
+
+    @Override
+    public int getByteLimit() {
+        return 512000;
+    }
+
+    @Override
+    public Map<Class<? extends WindowContextItem>, WindowContextItem> getWindowContextData() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void storeWindowContextData(Map<Class<? extends WindowContextItem>, WindowContextItem> windowContextData) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
 
 
