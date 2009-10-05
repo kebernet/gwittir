911c60525cf85d4323adb4a9abd3c536356dd591
kebernet <kebernet@d620f1b1-8330-0410-a80d-d7860aac830e>
2009-10-05 22:02:01 +0000

    Defensive commit. Work in progress.
    git-svn-id: http://gwittir.googlecode.com/svn/trunk@376 d620f1b1-8330-0410-a80d-d7860aac830e

---
+++ gwittir-core/src/main/java/com/totsp/gwittir/rest/server/XRESTFilter.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.totsp.gwittir.rest.server;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+/**
+ *
+ * @author rcooper
+ */
+public class XRESTFilter implements Filter {
+
+    public void init(FilterConfig filterConfig) throws ServletException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void destroy() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
