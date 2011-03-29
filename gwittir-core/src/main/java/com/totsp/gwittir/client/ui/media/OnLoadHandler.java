19a1b74f5c359cbfd7c973f0d4d222ab2dc96204
kebernet@gmail.com <kebernet@gmail.com@d620f1b1-8330-0410-a80d-d7860aac830e>
2011-03-29 19:41:20 +0000

    Fix for bound table and some cleanup.
    git-svn-id: http://gwittir.googlecode.com/svn/trunk@437 d620f1b1-8330-0410-a80d-d7860aac830e

--- gwittir-core/src/main/java/com/totsp/gwittir/client/ui/media/OnLoadHandler.java
+++
@@ -1,8 +0,0 @@
-package com.totsp.gwittir.client.ui.media;
-
-public interface OnLoadHandler {
-
-    public void onLoad();
-
-    public void onError();
-}
--- gwittir-core/src/main/java/com/totsp/gwittir/client/ui/media/ReadyState.java
+++
@@ -1,6 +0,0 @@
-package com.totsp.gwittir.client.ui.media;
-
-public enum ReadyState {
-
-    NONE, LOADING, LOADED, ERROR
-}
